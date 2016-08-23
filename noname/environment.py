e3054264075f6ab6d185faa9c90a43fb8dea9230
김나영 <imtherealk@gmail.com>
2016-08-23 22:52:24 +0900

    Environment 구현

--- noname/environment.py
+++ noname/environment.py
@@ -1,2 +1,16 @@
 class Environment(object):
-    pass
+    def __init__(self, parent):
+        super().__init__()
+        self.parent = parent
+        self.variables = {}
+
+    def find(self, name):
+        if name.name in self.variables:
+            return self.variables[name.name]
+        elif self.parent is None:
+            raise NameError(name.name)
+        else:
+            return self.parent.find(name)
+
+    def set(self, name, value):
+        self.variables[name.name] = value
--- tests/test_environment.py
+++ tests/test_environment.py
@@ -1,32 +1,32 @@
 import unittest
 
-from noname import Environment
+from noname import Symbol, Environment
 
 
 class TestEnvironment(unittest.TestCase):
     def test_easy(self):
         env = Environment(None)
-        env.set('a', 1)
-        self.assertEqual(1, env.find('a'))
-        self.assertRaises(NameError, env.find, 'b')
-        env.set('b', 2)
-        self.assertEqual(2, env.find('b'))
+        env.set(Symbol('a'), 1)
+        self.assertEqual(1, env.find(Symbol('a')))
+        self.assertRaises(NameError, env.find, Symbol('b'))
+        env.set(Symbol('b'), 2)
+        self.assertEqual(2, env.find(Symbol('b')))
 
     def test_parent(self):
         root = Environment(None)
-        root.set('a', 1)
+        root.set(Symbol('a'), 1)
         env = Environment(root)
-        env.set('b', 2)
-        self.assertEqual(2, env.find('b'))
-        self.assertEqual(1, env.find('a'))
+        env.set(Symbol('b'), 2)
+        self.assertEqual(2, env.find(Symbol('b')))
+        self.assertEqual(1, env.find(Symbol('a')))
 
     def test_overwrite(self):
         root = Environment(None)
-        root.set('a', 1)
+        root.set(Symbol('a'), 1)
         env = Environment(root)
-        env.set('a', 2)
-        self.assertEqual(2, env.find('a'))
-        self.assertEqual(1, root.find('a'))
+        env.set(Symbol('a'), 2)
+        self.assertEqual(2, env.find(Symbol('a')))
+        self.assertEqual(1, root.find(Symbol('a')))
 
 
 if __name__ == '__main__':
