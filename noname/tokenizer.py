d3a9e5018c0084338d306dbbdeb63ea28db35c3a
Nayeong <imtherealk@gmail.com>
2016-10-30 16:47:43 +0900

    tokenizer를 method로 구현

--- noname/tokenizer.py
+++ noname/tokenizer.py
@@ -4,89 +4,93 @@ import string
 SINGLE_TOKENS = ['(', ')', "'"]
 
 
-def next_char(chars):
-    try:
-        return next(chars)
-    except StopIteration:
-        return None
-
-
-def skip_whitespaces(first, chars):
-    while first is not None and first in string.whitespace:
-        first = next_char(chars)
-    return first
-
-
-def next_number(first, chars):
-    number = first
-    first = next_char(chars)
-    while first is not None and first.isdigit():
-        number += first
-        first = next_char(chars)
-    return first, number
-
-
-def next_string(first, chars):
-    token = first
-    first = next_char(chars)
-    while first != '"':
-        if first == '\\':
-            token += first
-            first = next_char(chars)
-        token += first
-        first = next_char(chars)
-    token += first
-    first = next_char(chars)
-    return first, token
-
-
-def next_full_number(first, chars):
-    first, left = next_number(first, chars)
-    if first is not None and first == '.':
-        first = next_char(chars)
-        if first is None or not first.isdigit():
-            number = left + '.'
-        else:
-            first, right = next_number(first, chars)
-            number = left + '.' + right
-    else:
-        number = left
-    return first, number
-
-
-def next_symbol(first, chars):
-    symbol = first
-    nonsymbol_chars = SINGLE_TOKENS + list(string.whitespace) + ['"']
-    first = next_char(chars)
-    while first is not None and first not in nonsymbol_chars:
-        symbol += first
-        first = next_char(chars)
-    return first, symbol
-
-
-def next_token(first, chars):
-    first = skip_whitespaces(first, chars)
-
-    if first is None:
-        return None, None
-
-    if first in SINGLE_TOKENS:
-        token = first
-        first = next_char(chars)
-        return first, token
-    elif first.isdigit():
-        return next_full_number(first, chars)
-    elif first == '"':
-        return next_string(first, chars)
-    else:
-        return next_symbol(first, chars)
-
-
 def tokenize(source):
-    source = iter(source)
-    first = next(source)
-    while first is not None:
-        first, token = next_token(first, source)
-        if token is None:
-            break
-        yield token
+    tokenizer = Tokenizer(iter(source))
+    return tokenizer.tokenize()
+
+
+class Tokenizer(object):
+    def __init__(self, chars):
+        self.chars = chars
+        self.first = None
+        self.finished = False
+
+    def tokenize(self):
+        self.read_char()
+        while not self.finished:
+            token = self.next_token()
+            if token is None:
+                break
+            yield token
+
+    def read_char(self):
+        try:
+            self.first = next(self.chars)
+        except StopIteration:
+            self.first = None
+            self.finished = True
+
+    def skip_whitespaces(self):
+        while not self.finished and self.first in string.whitespace:
+            self.read_char()
+
+    def next_number(self):
+        number = self.first
+        self.read_char()
+        while not self.finished and self.first.isdigit():
+            number += self.first
+            self.read_char()
+        return number
+
+    def next_string(self):
+        token = self.first
+        self.read_char()
+        while self.first != '"':
+            if self.first == '\\':
+                token += self.first
+                self.read_char()
+            token += self.first
+            self.read_char()
+        token += self.first
+        self.read_char()
+        return token
+
+    def next_full_number(self):
+        left = self.next_number()
+        if not self.finished and self.first == '.':
+            self.read_char()
+            if self.finished or not self.first.isdigit():
+                number = left + '.'
+            else:
+                right = self.next_number()
+                number = left + '.' + right
+        else:
+            number = left
+        return number
+
+    def next_symbol(self):
+        symbol = self.first
+        nonsymbol_chars = SINGLE_TOKENS + list(string.whitespace) + ['"']
+        self.read_char()
+        while not self.finished and self.first not in nonsymbol_chars:
+            symbol += self.first
+            self.read_char()
+        return symbol
+
+    def next_token(self):
+        self.skip_whitespaces()
+
+        if self.finished:
+            return None
+
+        if self.first in SINGLE_TOKENS:
+            token = self.first
+            self.read_char()
+            return token
+        elif self.first.isdigit():
+            return self.next_full_number()
+        elif self.first == '"':
+            return self.next_string()
+        else:
+            return self.next_symbol()
+
